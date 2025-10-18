054bbf6e9605e86eb3ec60b23c8629c846555141
Alex <alex.podikoglou@gmail.com>
2025-10-18 23:02:00 +0300

    fix: use set instead of add header

--- internal/handlers/gif.go
+++ internal/handlers/gif.go
@@ -15,7 +15,7 @@ func GifHandler(writer http.ResponseWriter, req *http.Request) {
 	// there is *probably* a path traversal vulnerability hidden here
 	text := query.Get("text")
 
-	writer.Header().Add("Content-Type", "image/gif")
+	writer.Header().Set("Content-Type", "image/gif")
 
 	input := ffmpeg.Input(fmt.Sprintf("gifs/%s.gif", gif))
 	input = input.Drawtext(text, 100, 200, false)
